3c755f2673fcf9745217027f5c823eefd81607a4
Andres Movilla <andres_movilla1103@hotmail.com>
2019-09-11 00:44:17 -0500

    restored public folder, rebuilt site, fixed icons and titles

---
+++ christmaseve/build/precache-manifest.f8433b09cb31c696f014a37e3ec31ef3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "be122bbe86287aa6126b933692b162bf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1f638a79e292151b2d40",
+    "url": "/static/css/main.c589d893.chunk.css"
+  },
+  {
+    "revision": "c4baa1bca9c85292ff4f",
+    "url": "/static/js/2.04ae2048.chunk.js"
+  },
+  {
+    "revision": "1f638a79e292151b2d40",
+    "url": "/static/js/main.e08bfb13.chunk.js"
+  },
+  {
+    "revision": "19af8ec384c3bdd96798",
+    "url": "/static/js/runtime~main.b68a5922.js"
+  }
+]);
